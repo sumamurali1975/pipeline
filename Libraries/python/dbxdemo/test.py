29240ce09cf42e12ca15e41a669104c1ba89a9f2
deepakgupta0914 <111429184+deepakgupta0914@users.noreply.github.com>
2022-08-26 10:44:46 +0530

    Add files via upload

---
+++ Libraries/python/dbxdemo/test.py
@@ -0,0 +1,11 @@
+from testbook import testbook
+import pytest
+
+@testbook("./notebooks.ipynb", execute=0)
+def test_output(tb):
+    assert tb.cell_output_text(0) == "hello world"
+
+@testbook("./notebooks.ipynb", execute=True)
+def test_add(tb):
+    add = tb.get("add")
+    assert add(1, 2) == 3
