14d804994ec74c8dbf5ddb6499d9e8cf02a2b416
sumamurali1975 <111351378+sumamurali1975@users.noreply.github.com>
2022-09-08 19:39:07 +0530

    Update mymath.py

--- Notebooks/mymath.py
+++ Notebooks/mymath.py
@@ -12,3 +12,15 @@ def multiply(a, b):
 
 def divide(numerator, denominator):
     return float(numerator) / denominator
+
+class TestSubtract(unittest.TestCase):
+    """
+    Test the subtract function from the mymath library
+    """
+
+    def test_subtract_integers(self):
+        """
+        Test that subtracting integers returns the correct result
+        """
+        result = mymath.subtract(10, 8)
+        self.assertEqual(result, 2)
